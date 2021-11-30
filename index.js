7b70bfa69519d81b60dafa2d2db10b3d7f6e6ba4
juan1497 <juanhuertas1297@gmail.com>
2021-11-30 15:44:26 +0100

    fixed origins 3

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    // res.header('Access-Control-Allow-Origin', 'https://jungla-nine.vercel.app');
+    res.header('Access-Control-Allow-Origin', '*');
     next();
 });
 const origins=['http://localhost:4200','https://jungla-nine.vercel.app']
